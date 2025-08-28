ea8c26a2e4ab84071c28a868c94911f1fd8c1841
Love Singhal <groothoon31@gmail.com>
2025-08-29 00:54:34 +0530

    Add dependencies and implement Navbar component; remove unused layout files

--- src/app/chai/lemon/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function LemonPage(){
-    return (
-        <>
-            <h1 className="text-2xl text-center">Lemon</h1>
-        </>
-    )
-}
--- src/app/chai/page.tsx
+++
@@ -1,5 +0,0 @@
-export default function ChaiPage(){
-    return (
-        <h1 className="text-3xl text-center mt-8">Chai</h1>
-    )
-}
